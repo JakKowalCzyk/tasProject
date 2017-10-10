8bb701b7ae284a275746b902f04d814a798180b4
Jakub Kowalczyk <beufort@o2.pl>
2017-10-10 21:40:26 +0200

    library lombok used

--- java-api/src/main/java/com/dreamteam/api/model/http/user/User.java
+++ java-api/src/main/java/com/dreamteam/api/model/http/user/User.java
@@ -2,7 +2,9 @@ package com.dreamteam.api.model.http.user;
 
 import com.dreamteam.api.model.enums.RoleType;
 import com.dreamteam.api.model.http.HttpModel;
+import lombok.Data;
 
+@Data
 public class User extends HttpModel {
 
     private String email;
@@ -14,43 +16,4 @@ public class User extends HttpModel {
     public User() {
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public RoleType getRoleType() {
-        return roleType;
-    }
-
-    public void setRoleType(RoleType roleType) {
-        this.roleType = roleType;
-    }
-
-    public String getHashPassword() {
-        return hashPassword;
-    }
-
-    public void setHashPassword(String hashPassword) {
-        this.hashPassword = hashPassword;
-    }
 }
--- java-api/src/main/resources/import.sql
+++ java-api/src/main/resources/import.sql
@@ -1,3 +1,3 @@
 insert into user_bo(email, name, city, role_type, hash_password) VALUES ('email', 'kuba', 'poznan', 'ADMIN', '123');
 insert into user_bo(email, name, city, role_type, hash_password) VALUES ('email1', 'kuba1', 'poznan1', 'ADMIN', '1233');
-insert into car(name, brand, price, millage) VALUES ('s60', 'volvo', '20000', 200);
+insert into car(name, brand, price, millage, is_rented) VALUES ('s60', 'volvo', '20000', 200, FALSE );
