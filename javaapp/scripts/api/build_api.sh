6704eed9b1819a70f06701fcca31a909e38df47b
Kuba Kowalczyk <kubabrag12>
2017-10-18 11:10:34 +0200

    scripts

--- javaapp/scripts/api/build_api.sh
+++ javaapp/scripts/api/build_api.sh
@@ -1,6 +1,6 @@
 cd ~/komis/code/tasProject/javaapp/java-api
 
-git pull https://github.com/JakKowalCzyk/tasProject docker
+git pull https://github.com/JakKowalCzyk/tasProject master
 
 mvn install dockerfile:build
 mvn  dockerfile:push
