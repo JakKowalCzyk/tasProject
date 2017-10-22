990b60842330aed0a6a0af604eab5e6ec8eaf4ae
MrFlashf <krz.jozefowicz@gmail.com>
2017-10-23 00:24:54 +0200

    Fix bugs

--- mobile-app/src/components/components.module.ts
+++ mobile-app/src/components/components.module.ts
@@ -1,6 +1,6 @@
-import { NgModule } from '@angular/core';
-import { HeaderComponent } from './header/header.component';
-import { AdComponent } from './ad/ad.component';
+import { NgModule }         from '@angular/core';
+import { HeaderComponent }  from './header/header.component';
+import { AdComponent }      from './ad/ad.component';
 import { AddIconComponent } from './add-icon/add-icon.component';
 import { BrandPipe }        from "../pipes/brand/brand.pipe";
 import { OrderComponent }   from './order/order.component';
