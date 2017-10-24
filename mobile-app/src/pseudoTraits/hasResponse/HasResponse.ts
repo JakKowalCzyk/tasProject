75f2e80209a6fb951650a34c932d8e6c263b62f0
MrFlashf <krz.jozefowicz@gmail.com>
2017-10-24 13:12:38 +0200

    fix bugs

--- mobile-app/src/pseudoTraits/hasResponse/HasResponse.ts
+++ mobile-app/src/pseudoTraits/hasResponse/HasResponse.ts
@@ -4,12 +4,10 @@ export class HasResponse {
     constructor(private events  : Events) {}
 
     success(response : Object, event : string) {
-        console.log(event);
         this.events.publish(event, response);
     }
 
     error(response : any, event : string) {
         this.events.publish('error:' + event, { msg : response.message})
     }
-
 }
