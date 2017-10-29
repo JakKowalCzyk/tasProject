4018813d20a86d6bbe5bd30746da9f7c21be26a2
Igor <29488070+igormisiorny@users.noreply.github.com>
2017-10-29 18:19:58 +0100

    Navigation links & Login foundation

--- Tas/src/app/app.module.ts
+++ Tas/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { NnnComponent } from './nnn/nnn.component';
 import {RouterModule, Routes} from "@angular/router";
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
@@ -12,21 +11,23 @@ import { CarComponent } from './car/car.component';
 import { AboutFirmComponent } from './about-firm/about-firm.component';
 import { TermsComponent } from './terms/terms.component';
 import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
+import { OfferComponent } from './offer/offer.component';
 // import {routableComponents, RoutingModuleModule} from "./routing-module/routing-module.module";
 
 
 const appRoutes: Routes = [
-  { path: 'dzejkob', component: NnnComponent },
   { path: 'main', component: MainComponent},
   { path: 'about-firm', component: AboutFirmComponent},
   { path: 'contact', component: ContactComponent},
   { path: 'terms', component: TermsComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'offer', component: OfferComponent},
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
-    NnnComponent,
     HeaderComponent,
     FooterComponent,
     MainComponent,
@@ -34,6 +35,8 @@ const appRoutes: Routes = [
     AboutFirmComponent,
     TermsComponent,
     ContactComponent,
+    OfferComponent,
+    LoginComponent
     // routableComponents
   ],
   imports: [RouterModule.forRoot(
