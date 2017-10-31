5def5fc545f823544ebb969d1868ee6a147fa09b
Kuba Kowalczyk <kubabrag12>
2017-10-31 12:53:37 +0100

    Register works with api - no validation and displaying errors

--- Tas/src/app/register/register.component.ts
+++ Tas/src/app/register/register.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UserService} from "../user-service";
+import {User} from "../models/user";
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
+  providers: [UserService]
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+
+  constructor(private userService: UserService) {
+  }
+
+  registerUser(email: string, pass: string, repeat: string) {
+    if (pass == repeat) {
+      let user = new User(null, email, pass, "poznan", null, pass);
+      this.userService.registerUser(user);
+    }
+    else
+      console.log("wrong password")
+  }
 
   ngOnInit() {
   }
+
 
 }
