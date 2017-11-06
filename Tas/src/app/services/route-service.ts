45c372db7d78ed16e78cfbdae3ab017737e5b38f
MrFlashf <krz.jozefowicz@gmail.com>
2017-11-06 19:49:58 +0100

    fix routeservice

--- Tas/src/app/services/route-service.ts
+++ Tas/src/app/services/route-service.ts
@@ -1,30 +1,30 @@
 import {Injectable} from "@angular/core";
 
-@Injectable
+@Injectable()
 export class RouteService {
 
-  baseApi: string = "http://159.89.12.132:8080/api/";
+  baseApi : string = "http://159.89.12.132:8080/api/";
 
   routes = {
-    'cars': this.baseApi + 'car/',
-    'car': this.baseApi + 'car/',
-    'brands': this.baseApi + 'brand/',
-    'addCar': this.baseApi + 'car',
-    'addBrand': this.baseApi + 'brand',
-    'categories': this.baseApi + 'type/category/',
-    'filter': this.baseApi + 'car/filter',
-    'addPhoto': this.baseApi + 'car/',
+    'cars'          : this.baseApi + 'car/',
+    'car'           : this.baseApi + 'car/',
+    'brands'        : this.baseApi + 'brand/',
+    'addCar'        : this.baseApi + 'car',
+    'addBrand'      : this.baseApi + 'brand',
+    'categories'    : this.baseApi + 'type/category/',
+    'filter'        : this.baseApi + 'car/filter',
+    'addPhoto'      : this.baseApi + 'car/',
 
     //user
-    'register': this.baseApi + 'user',
-    'login': this.baseApi + 'user/me',
-    'logout': this.baseApi + 'user/logout',
+    'register'      : this.baseApi + 'user',
+    'login'         : this.baseApi + 'user/me',
+    'logout'        : this.baseApi + 'user/logout',
 
     //orders
-    'rent': this.baseApi + 'car/rented',
-    'user_orders': this.baseApi + 'car/rented/user/me',
-    'all_orders': this.baseApi + 'car/rented/',
-    'cancel_order': this.baseApi + 'car/rented/cancel/',
+    'rent'          : this.baseApi + 'car/rented',
+    'user_orders'   : this.baseApi + 'car/rented/user/me',
+    'all_orders'    : this.baseApi + 'car/rented/',
+    'cancel_order'  : this.baseApi + 'car/rented/cancel/',
   };
 
 }
