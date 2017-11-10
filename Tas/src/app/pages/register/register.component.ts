d7782c3f61835c71fbef14662ccd0a0f0d7707d9
Kuba Kowalczyk <kubabrag12>
2017-11-10 12:17:28 +0100

    Register design

--- Tas/src/app/pages/register/register.component.ts
+++ Tas/src/app/pages/register/register.component.ts
@@ -18,6 +18,7 @@ export class RegisterComponent implements OnInit {
               private formBuilder: FormBuilder) {
     this.registerForm = this.formBuilder.group({
       name: ['', Validators.compose([Validators.required])],
+      city: ['', Validators.compose([Validators.required])],
       email: ['', Validators.compose([Validators.required, Validators.pattern(/^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i)])],
       password: ['', Validators.compose([Validators.required])],
       repeatPass: ['', Validators.compose([Validators.required, matchOtherValidator('password')])]
@@ -28,9 +29,17 @@ export class RegisterComponent implements OnInit {
     if (!this.registerForm.valid) {
       return;
     } else {
-      let user = new User(null, this.registerForm.get('email').value, this.registerForm.get('name').value, "poznan", null, this.registerForm.get('password').value);
+      let user = new User(null, this.registerForm.get('email').value, this.registerForm.get('name').value, this.registerForm.get('city').value, null, this.registerForm.get('password').value);
       this.userService.registerUser(user);
     }
+  }
+
+  isRepeatPasswordValid(): any {
+    return this.registerForm.get('password').value == this.registerForm.get('repeatPass').value;
+  }
+
+  isUserPresent() {
+    return this.userService.user != null;
   }
 
   ngOnInit() {
